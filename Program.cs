7838929b8f3b56496fcf260082e90afe83067511
yotekle <tekleab2000@gmail.com>
2022-10-28 20:54:56 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mycsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Tekleab!");
+        }
+    }
+}
